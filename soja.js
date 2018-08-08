4a9c97da09f9e3c67df8f991a0146efa47e5d52f
sinistroagro <sinistroagro@a7.local>
2018-08-08 17:59:40 -0300

    fix json

--- soja.js
+++ soja.js
@@ -21,3 +21,18 @@ lista_cultivares.push(criarCultivar('AV GURIA RR', 'AVANTI SEEDS', 'GRUPO I'))
 const cult = []
 
 lista_cultivares.push(criarCultivar(cult[0], cult[1], cult[2]))
+
+const fs = require('fs')
+
+const caminho = __dirname + '/macrorregiao1.json'
+
+// sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const data = JSON.parse(conteudo)
+    data.macrorregiao1.forEach(function(nam, indice) {
+        console.log(`${indice + 1}) ${nam.regiao_endafoclimatica.id}`)
+    })
+})
